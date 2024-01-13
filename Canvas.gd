be281c29f6218764d8b402f2e995a06b4cc8b8f4
Marcus Völker <voelker@embedded.rwth-aachen.de>
2024-01-13 13:08:13 +0100

    More debug renderers

--- Canvas.gd
+++ Canvas.gd
@@ -3,11 +3,14 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	num = 0.0
 
+
+var num = 0.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var shader : ShaderMaterial = material
-	shader.set_shader_parameter("p1",Vector2(-PI/4,-PI/4))
-	shader.set_shader_parameter("p2",Vector2(PI/4,PI/2))
+	num = num + delta
+	shader.set_shader_parameter("p1",Vector2(-PI/2,0))
+	shader.set_shader_parameter("p2",Vector2(-PI/3,num))
